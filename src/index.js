a6d4854eaca39b2cd56e927f66ca58d210e355ac
Alivarik <45916819+Alivarik@users.noreply.github.com>
2019-10-05 20:24:08 +0300

    index.js edit solution

--- src/index.js
+++ src/index.js
@@ -1,21 +1,49 @@
-module.exports = function check(str, bracketsConfig) {
-    let count = 0;
-    let i = 0;
-    let promStr = "";
+const config1 = [
+    ["(", ")"]
+];
+const config2 = [
+    ["(", ")"],
+    ["[", "]"]
+];
+const config3 = [
+    ["(", ")"],
+    ["[", "]"],
+    ["{", "}"]
+];
+const config4 = [
+    ["|", "|"]
+];
+const config5 = [
+    ["(", ")"],
+    ["|", "|"]
+];
+const config6 = [
+    ["1", "2"],
+    ["3", "4"],
+    ["5", "6"],
+    ["7", "7"],
+    ["8", "8"]
+];
+const config7 = [
+    ["(", ")"],
+    ["[", "]"],
+    ["{", "}"],
+    ["|", "|"]
+];
+
+function check(str, bracketsConfig) {
     let array = [];
     let stopSignal = 1;
-    while ((stopSignal = 1)) {
+    while (stopSignal == 1) {
         stopSignal = 0;
-        bracketsConfig.forEach(function(element, index) {
-            array = str.split(
-                bracketsConfig[index][0] + bracketsConfig[index][1]
-            );
-        });
-        if (array.length > 1) {
-            count = count + 1;
-            stopSignal = 1;
-            promStr = array.join("");
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            array = str.split(bracketsConfig[i][0] + bracketsConfig[i][1]);
+            if (array.length > 1) {
+                stopSignal = 1;
+                str = array.join("");
+            }
         }
     }
-    return promStr == "" ? true : false;
-};
+    return str == "" ? true : false;
+}
+console.log(check("((()))()", config1));
