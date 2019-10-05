5f40cbbe21d1a37aba483eda834452c0eb9091e5
Alivarik <45916819+Alivarik@users.noreply.github.com>
2019-10-05 21:58:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,4 @@
-const config1 = [
-    ["(", ")"]
-];
-const config2 = [
-    ["(", ")"],
-    ["[", "]"]
-];
-const config3 = [
-    ["(", ")"],
-    ["[", "]"],
-    ["{", "}"]
-];
-const config4 = [
-    ["|", "|"]
-];
-const config5 = [
-    ["(", ")"],
-    ["|", "|"]
-];
-const config6 = [
-    ["1", "2"],
-    ["3", "4"],
-    ["5", "6"],
-    ["7", "7"],
-    ["8", "8"]
-];
-const config7 = [
-    ["(", ")"],
-    ["[", "]"],
-    ["{", "}"],
-    ["|", "|"]
-];
-
-function check(str, bracketsConfig) {
+module.exports = function check(str, bracketsConfig) {
     let array = [];
     let stopSignal = 1;
     while (stopSignal == 1) {
@@ -45,5 +12,4 @@ function check(str, bracketsConfig) {
         }
     }
     return str == "" ? true : false;
-}
-console.log(check("())(", config1));
+};
